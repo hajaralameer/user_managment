2baeb56d2909bd28ecd7eaf6a4c839596f3af1d5
hajaralameer <159311669+hajaralameer@users.noreply.github.com>
2024-08-27 14:24:01 +0300

    Delete wchatgpt directory
    Delete wchatgpt directory

--- wchatgpt/about.php
+++
@@ -1,9 +0,0 @@
-<?php include('header.php'); ?>
-
-<div class="about-page">
-    <h2>About Us</h2>
-    <img src="images/about.jpg" alt="About Us">
-    <p>We are a team of dedicated professionals...</p>
-</div>
-
-<?php include('footer.php'); ?>
